7f917bd77ea99ed935fcc92aca1524b97c045066
James Penn <JamesLockyerPenn@gmail.com>
2025-10-19 07:39:15 +0800

    feat: Added storage device resource.

---
+++ Systems/Resources/Devices/StorageResource.cs
@@ -0,0 +1,49 @@
+using Godot;
+using Godot.Collections;
+using Khepri.Entities.Actors;
+using Khepri.Entities.Items;
+using System;
+
+namespace Khepri.Resources.Devices
+{
+    /// <summary> The data component of a chest or chest-adjacent object. </summary>
+    [GlobalClass]
+    public partial class StorageResource : DeviceResource
+    {
+        /// <summary> The grid size of the entity's inventory. </summary>
+        [ExportGroup("Settings")]
+        [Export] public Vector2I InventorySize { get; private set; } = new Vector2I(10, 10);
+
+
+        /// <summary> A reference to the entity's inventory component. </summary>
+        public EntityInventory Inventory { get; private set; }
+
+
+        /// <summary> The data component of a chest or chest-adjacent object. </summary>
+        public StorageResource()
+        {
+            Inventory = new EntityInventory(InventorySize);
+        }
+
+
+        /// <inheritdoc/>
+        public override void Use(ActorNode activatingBeing)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <inheritdoc/>
+        public override void Deserialise(Dictionary<String, Variant> data)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        /// <inheritdoc/>
+        public override Dictionary<String, Variant> Serialise()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
