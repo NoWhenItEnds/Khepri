8acc47eef17e8e99d03a8671aa8797cf531fbdb7
James Penn <JamesLockyerPenn@gmail.com>
2026-06-07 09:25:17 +0800

    ...

--- Khepri/Systems/UI/World/Tabs/TabPanel.cs
+++ Khepri/Systems/UI/World/Tabs/TabPanel.cs
@@ -1,5 +1,6 @@
 using Godot;
 using Khepri.Entities;
+using Khepri.Managers;
 using Khepri.Rooms;
 
 namespace Khepri.UI.World.Tabs
@@ -16,6 +17,23 @@ namespace Khepri.UI.World.Tabs
 
         /// <summary> The panel that shows a map of the game world. </summary>
         [Export] private OverworldPanel _overworldPanel = null!;
+
+
+        /// <inheritdoc/>
+        public override void _Ready()
+        {
+            _overworldPanel.RoomSelected += OnRoomSelected;
+        }
+
+
+        /// <summary> Hands the room the player selected to their controller as a move request. </summary>
+        /// <remarks> The controller turns the click into an action the <see cref="TurnManager"/> performs on the player's next turn, rather than the UI mutating the world directly; selecting a room that is not directly connected yields a move that simply fails when performed, leaving the player free to choose again. </remarks>
+        /// <param name="destination"> The room the player clicked. </param>
+        private void OnRoomSelected(Room destination)
+        {
+            TurnManager.Instance!.Player.Select(destination);
+        }
+
 
         /// <summary> Force the UI element to reflect the current game's state. </summary>
         /// <param name="playerEntity"> The currently controlled player entity. </param>
